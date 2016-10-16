c111aebc38ab6aaf78b6f1ad627409943e01ac49
theory friction practice <jackson.brebnor@gmail.com>
2016-10-16 13:10:25 -0400

    nodeclass

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
-module.exports.Board = require('./board');
+module.exports.Node = require('./node');
 module.exports.Column = require('./board');
+module.exports.Board = require('./board');
+
 // module.exports.Game = require('./game');
 // module.exports.Player = require('./player');
--- spec/board.spec.js
+++ spec/board.spec.js
@@ -8,16 +8,16 @@ describe('Board', function() {
 		myBoard = Board.spawn();
 	});
 
-	it('is a grid with 7 columns and 6 rows', function() {
-		// console.log(myBoard);
-		// GameGraph.connectAdjacents(myBoard);
-		expect(myBoard instanceof Map).toBeTrue();
-	});
+	// it('is a grid with 7 columns and 6 rows', function() {
+	// 	// console.log(myBoard);
+	// 	// GameGraph.connectAdjacents(myBoard);
+	// 	expect(myBoard instanceof Map).toBeTrue();
+	// });
 
-	describe('columns', function() {
-		it('returns an array of columns', function() {
-			// console.log(Board.columns(myBoard));
-			expect(Board.columns(myBoard)).toBeArray();
-		});
-	});
+	// describe('columns', function() {
+	// 	it('returns an array of columns', function() {
+	// 		// console.log(Board.columns(myBoard));
+	// 		expect(Board.columns(myBoard)).toBeArray();
+	// 	});
+	// });
 });
