49bbf15456e34feb280014f53f4d8f4ccc43c573
theory friction practice <jackson.brebnor@gmail.com>
2016-10-16 13:01:32 -0400

    minor

--- app/board.js
+++ app/board.js
@@ -4,11 +4,11 @@ const { initCells, cIDs, nodesByColumn, fromElements, mergeEdges, } = Grid;
 const spawn = () => initCells(7, 6);
 
 const merge = (g0 = new Map, g1 = new Map) =>
-	[ ...g1 ].filter(([k, v]) => !g0.has(k)).reduce(mergeEdges, g0);
+    [...g1].filter(([k, v]) => !g0.has(k)).reduce(mergeEdges, g0);
 
-const columns = (board) => [ ...cIDs(board) ]
-	.map(nodesByColumn(board))
-	.map(cells => fromElements(...cells));
+const columns = (board) => [...cIDs(board)]
+    .map(nodesByColumn(board))
+    .map(cells => fromElements(...cells));
 // .map(omniGraph)
 // .reduce(merge, new Map);
 
