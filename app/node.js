6fd3c6d977a28fc8a7011fed87ebc9595a915242
theory friction practice <jackson.brebnor@gmail.com>
2016-10-25 14:32:47 -0400

    checks for null in case of zero as player id

--- app/node.js
+++ app/node.js
@@ -2,10 +2,10 @@ const { Cell } = require('game_grid');
 const { spawn: cSpawn } = Cell;
 
 const spawn = (c = 0, r = 0, player = null) =>
-	Object.assign(cSpawn(c, r), { player });
+    Object.assign(cSpawn(c, r), { player });
 
 const player = ({ player = null }) => player;
-const isFree = ({ player }) => !player;
+const isFree = ({ player = null }) => player == null;
 const samePlayer = ({ player: p0 }) => ({ player: p1 }) => p0 === p1;
 
 module.exports = Object.assign({}, Cell, { spawn, isFree, player, samePlayer });
