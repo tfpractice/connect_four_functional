9a3e6121ca0bdbf8de00cc1e1c6567b5f5101ae2
theory friction practice <jackson.brebnor@gmail.com>
2016-10-26 19:51:36 -0400

    removes FGT dependencyfrom specs

--- spec/helpers/spec_helper.js
+++ spec/helpers/spec_helper.js
@@ -1,12 +1,8 @@
 beforeAll(function() {
-    require('jasmine-expect');
-    app = require('../../index');
-    GT = require('functional_graph_theory');
-    GG = require('game_grid');
-    // ({ Utils: { Strings: STR }, } = GT);
-    // ({ Cell, Grid, GameGraph, Player: GP, Game } = GG);
-    ({ Node, Board, Player, Game, Utils } = app);
-    ({ Commands: Comms, Strings: STR } = Utils);
+	require('jasmine-expect');
+	app = require('../../index');
+	({ Node, Board, Player, Game, Utils } = app);
+	({ Commands: Comms, Strings: STR } = Utils);
 });
 
 beforeEach(function() {});
