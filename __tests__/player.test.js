0ebbfc5ea4583d828bb47e5b386be556ef79c623
theory friction practice <jackson.brebnor@gmail.com>
2017-01-08 18:32:04 -0500

    player constructor

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,9 +1,17 @@
 import 'jasmine-expect';
-xdescribe('Player', () => {
-  it('returns a new map with altered keys', () => {
-    expect(true).toBeTruthy();
-  });
 
+import player, { claim, decrementScore, incrementScore, name, resetScore, score, } from 'src/player';
+const dick = player('Dick');
+const jane = player('Jane');
+
+// const myBoard = board(...genNodes());
+// const myNodes = nodes(myBoard);
+// const col0 = nodesByColumn(myBoard)(0);
+// const col1 = nodesByColumn(myBoard)(1);
+// const [c0r0, c0r1, c0r2, c0r3, c0r4, c0r5] = col0;
+// const [c1r0, c1r1, c1r2, c1r3, c1r4, c1r5] = col1;
+
+describe('Player', () => {
 // 	beforeAll(function() {
 // 		console.log('\n.........Player Spec.........');
 // 		({ Player: PR, Node } = app);
@@ -18,11 +26,12 @@ xdescribe('Player', () => {
 // 		col1 = [c1r0, c1r1, c1r2, c1r3, c1r4, c1r5] = byCol(myBoard)(1);
 // 	});
 //
-// 	describe('spawn', () => {
-// 		it('returns an object with a ', () => {
-// 			expect(dick).toBeObject();
-// 		});
-// 	});
+  describe('default', () => {
+    it('returns an object with a name and score ', () => {
+      expect(dick).toBeObject();
+    });
+  });
+
 // 	describe('name ', () => {
 // 		it('retrieves the name attribute', () => {
 // 			expect(PR.name(dick)).toBe('Dick');
--- src/player.js
+++ src/player.js
@@ -1,17 +1,19 @@
-const spawn = (name = '') => ({ name, score: 0 });
-const name = ({ name }) => name;
-const score = ({ score }) => score;
-const resetScore = (player) => player.wins = 0;
-const incrementScore = ({ score }) => ++score;
-const decrementScore = ({ score }) => --score;
-const claim = (player = null) => (n) => n && Object.assign(n, { player });
+export const playerInit = { name: '', score: 0 };
+export default (name = '', score = 0) => ({ name, score });
+export const name = ({ name }) => name;
+export const score = ({ score }) => score;
+export const resetScore = player => player.wins = 0;
+export const incrementScore = ({ score }) => ++score;
+export const decrementScore = ({ score }) => --score;
+export const claim = (player = null) => n => n && Object.assign(n, { player });
 
-module.exports = {
-	spawn,
-	name,
-	score,
-	resetScore,
-	incrementScore,
-	decrementScore,
-	claim,
-};
+//
+// module.exports = {
+//   spawn,
+//   name,
+//   score,
+//   resetScore,
+//   incrementScore,
+//   decrementScore,
+//   claim,
+// };
