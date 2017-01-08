4715115b6231e28b8b93fd7f5d6d94f1e4152511
theory friction practice <jackson.brebnor@gmail.com>
2017-01-08 01:21:21 -0500

    tests kvMap util

--- __tests__/commands.spec.js
+++ __tests__/commands.spec.js
@@ -1,6 +1,8 @@
 import 'jasmine-expect';
-import * as comms from 'src/utils/commands';
-console.log('comms', comms);
+import { kvMap, } from 'src/utils';
+
+// console.log('comms', comms);
+
 const cSet = new Set([ 1, 2, 3, 4 ]);
 const cMap = new Map().set(8, 1).set(7, 2).set(6, 3).set(5, 4);
 const cArr = [ 0, 11, 22, 33, 44 ];
