c932d4a371c6abcd250ba6ca182235bd9203eb55
theory friction practice <jackson.brebnor@gmail.com>
2017-02-13 16:34:05 -0500

    node.test

--- src/node.js
+++ src/node.js
@@ -1,8 +1,14 @@
-import { node, } from 'game_grid';
+import { Node, } from 'game_grid';
+const { assign } = Object;
+
+const defP = { player: null };
+
+export const node = (c = 0, r = 0, player = null) =>
+ Object.assign(Node.node(c, r), { player });
 
 export default (c = 0, r = 0, player = null) =>
 Object.assign(node(c, r), { player });
 
-export const player = ({ player = null }) => player;
-export const isFree = ({ player = null }) => player === null;
+export const player = ({ player } = defP) => player;
+export const isFree = ({ player } = defP) => player === null;
 export const samePlayer = ({ player: p0 }) => ({ player: p1 }) => p0 === p1;
