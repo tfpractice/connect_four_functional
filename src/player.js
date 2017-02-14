b6d5217d8681aef2840e65aa8d3210048acc2e8e
theory friction practice <jackson.brebnor@gmail.com>
2017-02-13 19:51:29 -0500

    player id

--- src/player.js
+++ src/player.js
@@ -1,9 +1,10 @@
 export const playerInit = { name: '', score: 0 };
 export default (name = '', score = 0) => ({ name, score });
 
-export const player = (name = '', score = 0) => ({ name, score });
+export const player = (id = null, name = id, score = 0) => ({ name, score, id });
 export const name = ({ name } = playerInit) => name;
 export const score = ({ score } = playerInit) => score;
+export const getID = ({ id }) => id;
 export const resetScore = player => player.wins = 0;
 export const incrementScore = ({ score }) => ++score;
 export const decrementScore = ({ score }) => --score;
