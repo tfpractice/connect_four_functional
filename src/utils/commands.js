1b5c8c9070ae9bab4c891bb34ee4382eadb8c6a8
theory friction practice <jackson.brebnor@gmail.com>
2017-02-16 01:42:15 -0500

    removes utils dir

--- src/utils/commands.js
+++
@@ -1,6 +0,0 @@
-import { collections, } from 'turmeric-utils';
-
-const { addBinMap, spread, } = collections;
-
-export const kvMap = (map = new Map) => fn =>
-  spread(map).map(([ k, v = k ]) => [ k, fn(v) ]).reduce(addBinMap, new Map);
--- src/utils/strings.js
+++
@@ -1,8 +0,0 @@
-const { Utils: { Strings } } = require('game_grid');
-const { spread, spreadV, spreadKV, addMap } = require('./commands');
-
-const compString = (set) => '\n' + `${spread(set).join(' => ')}`;
-const pairString = ([k, v]) => `${k} :: ${spread(v).map(compString)}`;
-const mapString = (coll) => spreadKV(coll).map(pairString);
-
-module.exports = Object.assign({}, Strings, { compString, pairString, mapString });
