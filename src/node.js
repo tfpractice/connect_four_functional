ce6f602154f25d8ffff1ca3df24038ddf55c5e35
theory friction practice <jackson.brebnor@gmail.com>
2017-02-15 20:14:57 -0500

    obj.ass node

--- src/node.js
+++ src/node.js
@@ -1,11 +1,11 @@
 import { Node, } from 'game_grid';
 import { id, } from './player';
 const { column, row, node: gNode } = Node;
-const { assign: asg } = Object;
 
 const defP = { player: null };
 
-export const node = (c = 0, r = 0, player = null) => asg(gNode(c, r), { player });
+export const node = (c = 0, r = 0, player = null) =>
+  Object.assign({}, gNode(c, r), { player });
 
 export const player = ({ player } = defP) => player;
 export const setPlayer = p => n => node(column(n), row(n), id(p));
