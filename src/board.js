cd0b7d2b0a227a33deccabc10ab8eaec7f318066
theory friction practice <jackson.brebnor@gmail.com>
2017-02-15 22:44:43 -0500

    board final

--- src/board.js
+++ src/board.js
@@ -1,20 +1,19 @@
-import { asSet, flattenBin, some, spread, } from 'fenugreek-collections';
+import { some, } from 'fenugreek-collections';
 import { Graph, } from 'graph-curry';
 import { Components, Grid, } from 'game_grid';
-import { copy as copyN, isFree, node, samePlayer, setPlayer, } from './node';
-import { byExcess, byPlayer, exceeds, hasFree, nextFree, } from './filter';
 import { id, } from './player';
+import { copy as copyN, } from './node';
+import { byExcess, byPlayer, exceeds, } from './filter';
 
 const { graph, nodes, } = Graph;
-const { genNodes: gen, grid, } = Grid;
+const { genNodes: gen, } = Grid;
 const { omniComps, } = Components;
 
 export const genNodes = (c = 7, r = 6) => gen(c, r).map(copyN);
 export const board = (c = 7, r = 6) => graph(...genNodes(c, r));
-export const copy = b => graph(...nodes(b));
+export const copy = b => graph(...nodes(b).map(copyN));
 
-export const playerNodes = board => p => byPlayer(nodes(board))(id(p));
-
+export const playerNodes = b => p => byPlayer(nodes(b))(id(p));
 export const playerGraph = b => p => graph(...playerNodes(b)(p));
 
 export const hasWinComp = (g, n = 3) => some(omniComps(g))(exceeds(n));
